7807494919113d0c347652fe9a9b75974da54005
l0u0h0 <l0u0h0@gmail.com>
2023-11-22 13:56:38 +0900

    :sparkles: 메타 데이터 추가 및 아이콘 추가

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,18 @@ export default function Home() {
       <Head>
         <title>l0u0h0</title>
         <meta name="description" content="l0u0h0's Front-end Portfolio Web" />
+        <meta name="author" content="l0u0h0" />
+
+        <meta
+          property="og:image"
+          content="https://st4.depositphotos.com/21936426/24029/i/450/depositphotos_240297282-stock-photo-concepts-working-readiness-notebooks-coffee.jpg"
+        />
+        <meta
+          property="og:description"
+          content="l0u0h0's Portfolio Web
+          learning for Front-end Developer"
+        />
+        <meta property="og:title" content="l0u0h0's" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
