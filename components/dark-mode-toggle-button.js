dcc1e9f4d41b96b09018192f82b4fd1b725efdaa
l0u0h0 <l0u0h0@gmail.com>
2023-11-22 11:16:39 +0900

    :sparkles: 헤더 수정

--- components/dark-mode-toggle-button.js
+++ components/dark-mode-toggle-button.js
@@ -10,7 +10,7 @@ export default function DarkModeToggleButton() {
   return (
     <>
       <button
-        className="inline-flex items-center bg-gray-100 hover:bg-gray-50 dark:bg-slate-600 dark:hover:bg-slate-700 dark:text-slate-900 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 hover:text-orange-500 dark:hover:text-yellow-500 rounded text-base mt-4 md:mt-0"
+        className="inline-flex items-center bg-gray-100 hover:bg-gray-50 dark:bg-slate-600 dark:hover:bg-slate-700 dark:text-slate-900 border-0 py-1 px-3 focus:outline-none hover:text-orange-500 dark:hover:text-yellow-500 rounded text-base"
         type="button"
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       >
--- components/header.js
+++ components/header.js
@@ -4,14 +4,14 @@ import DarkModeToggleButton from "./dark-mode-toggle-button";
 export default function Header() {
   return (
     <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <div className="container mx-auto flex flex-wrap p-5 px-20 flex-col md:flex-row items-center">
         <Link href="/">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
-              className="w-10 h-10"
+              className="w-16 h-16"
             >
               <path
                 fillRule="evenodd"
@@ -20,20 +20,23 @@ export default function Header() {
               />
             </svg>
 
-            <span className="ml-3 text-xl">AlPong PortFolio</span>
+            <span className="ml-3 text-3xl font-bold">l0u0h0's</span>
           </a>
         </Link>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        <nav className="md:ml-auto flex flex-wrap items-center text-lg justify-center">
           <Link href="/">
             <a className="mr-5 hover:text-gray-900">홈</a>
           </Link>
           <Link href="/projects">
             <a className="mr-5 hover:text-gray-900">프로젝트</a>
           </Link>
-          <a className="mr-5 hover:text-gray-900">연락하기</a>
+          {/* contact 페이지 링크 예정 */}
+          <Link href="/">
+            <a className="mr-5 hover:text-gray-900">연락하기</a>
+          </Link>
+          {/* 다크모드 토글버튼 */}
+          <DarkModeToggleButton />
         </nav>
-        {/* dark mode toggle button */}
-        <DarkModeToggleButton />
       </div>
     </header>
   );
