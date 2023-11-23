45afd402473f065d8aa4c36b593da33dac22f5d3
l0u0h0 <l0u0h0@gmail.com>
2023-11-23 17:43:32 +0900

    :sparkles: 프로필 페이지 반응형 적용

--- pages/profile.js
+++ pages/profile.js
@@ -14,10 +14,10 @@ export default function Profile() {
         <div className="w-full flex flex-col mx-6 md:mr-6 text-center md:text-left items-start">
           <div className="w-full title-font text-start">
             <h1 className="text-2xl sm:text-3xl">☺️ About Me</h1>
-            <hr className="min-w-[246px] md:min-w-[720px] mt-4" />
+            <hr className="min-w-[246px] mt-4 border-2 dark:border-1 border-zinc-900 dark:border-zinc-300" />
           </div>
-          <div className="flex">
-            <div className="w-40 m-10">
+          <div className="flex flex-wrap md:flex-nowrap">
+            <div className="w-40 min-w-[10rem] m-auto mt-10 md:m-10">
               <Image
                 className="rounded-md"
                 src={`/Images/profile-image.jpg`}
@@ -31,24 +31,30 @@ export default function Profile() {
               />
             </div>
             <div className="m-10 ml-0">
-              <ul className="grid grid-cols-1 md:grid-cols-2 leading-relaxed text-lg text-start">
+              <ul className="lg:grid lg:grid-cols-2 lg:gap-2 leading-relaxed flex flex-wrap gap-x-10 text-lg text-start">
                 <li className="mb-2">
-                  <b className="text-xl">이름</b> <br /> 이유한
+                  <b className="text-xl font-jalnan">이름</b> <br /> 이유한
                 </li>
                 <li className="mb-2">
-                  <b className="text-xl">이메일</b> <br /> l0u0h0@gmail.com
+                  <b className="text-xl font-jalnan">이메일</b> <br />{" "}
+                  l0u0h0@gmail.com
                 </li>
-                <li className="mb-2">
-                  <b className="text-xl">Velog</b> <br />{" "}
+                <li className="mb-2 overflow-clip">
+                  <b className="text-xl font-jalnan">Velog</b> <br />{" "}
                   <a href="https://velog.io/@l0u0h0" target="_blank">
                     https://velog.io/@l0u0h0
                   </a>
                 </li>
-                <li className="mb-2">
-                  <b className="text-xl">Git</b> <br />{" "}
+                <li className="mb-2 overflow-clip">
+                  <b className="text-xl font-jalnan">Git</b> <br />{" "}
                   <a href="https://github.com/l0u0h0" target="_blank">
                     https://github.com/l0u0h0
                   </a>
+                </li>
+                <li className="mb-2">
+                  <b className="text-xl font-jalnan">학력</b> <br /> 백석대학교
+                  졸업 <small className="mt-0 mb-1">2017.03 ~ 2023.02</small>
+                  <br /> 환일고등학교 졸업 <small>2014.03 ~ 2017.02</small>
                 </li>
               </ul>
             </div>
@@ -57,10 +63,10 @@ export default function Profile() {
         <div className="w-full flex flex-col mx-6 md:mr-6 text-center md:text-left items-start">
           <div className="w-full title-font text-start">
             <h1 className="text-2xl sm:text-3xl">💼 Experience</h1>
-            <hr className="min-w-[246px] md:min-w-[720px] mt-4" />
+            <hr className="min-w-[246px] mt-4 border-2 dark:border-1 border-zinc-900 dark:border-zinc-300" />
           </div>
-          <div className="flex">
-            <div className="w-40 m-10">
+          <div className="flex flex-wrap md:flex-nowrap">
+            <div className="w-40 min-w-[10rem] m-auto mt-10 md:m-10">
               <Image
                 className="rounded-md"
                 src={`/Images/SSAFY-image.png`}
@@ -76,15 +82,17 @@ export default function Profile() {
             <div className="m-10 ml-0">
               <ul className="leading-relaxed text-lg text-start font-jalnangothic">
                 <li className="mb-2">
-                  <b className="text-xl font-jalnan">삼성 청년 SW 아카데미</b> <br /> 9기 자바 전공자반
+                  <b className="text-xl font-jalnan">삼성 청년 SW 아카데미</b>{" "}
+                  <br /> 9기 자바 전공자반
                   <li className="pl-4 text-base font-sans">
-                  알고리즘을 기반으로 한 코딩 교육 과정을 통해 구조적이고 효율적인 코드 작성
+                    - 알고리즘을 기반으로 한 코딩 교육 과정을 통해 구조적이고
+                    효율적인 코드 작성
                   </li>
                   <li className="pl-4 text-base font-sans">
-                  팀프로젝트에 대한 경험, 신기술 및 팀원들과의 소통, 컨벤션 규정 등 협업 경험
+                    - 팀프로젝트에 대한 경험, 신기술 및 팀원들과의 소통, 컨벤션
+                    규정 등 협업 경험
                   </li>
                 </li>
-                
               </ul>
             </div>
           </div>
