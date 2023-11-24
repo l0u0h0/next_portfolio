90b98609a12583dc3e0bd74417b9a5c11e8db712
l0u0h0 <l0u0h0@gmail.com>
2023-11-24 17:44:51 +0900

    :sparkles: 프로필 및 홈 화면 문구 대폭 수정

--- components/Home/hero.js
+++ components/Home/hero.js
@@ -6,29 +6,22 @@ export default function Hero() {
     <div className="flex flex-wrap-reverse md:flex-nowrap justify-center">
       <div className="md:basis-1/2 lg:w-1/2 md:w-1/2 mx-4 px-2 md:pr-6 lg:pr-10 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-          이유한,&nbsp;
+          서비스 최적화를 꿈꾸는&nbsp;
           <br className="hidden lg:inline-block" />
-          Front-End Developer
+          이유한입니다.
         </h1>
         <ul className="mb-8 ml-6 leading-relaxed text-lg text-left list-disc">
           <li>
-            팀 구성원들과의 <b>커뮤니케이션</b>을 중요시하며 <b>모르는 것</b>을
-            해결할 때, <b>새로운 것</b>을 배워나갈 때 <b>즐거움</b>을 느끼는
-            주니어 개발자입니다.
+            서비스 <b>최적화</b>를 위해 <b>고민하고 노력하는 개발자</b>를 목표로 나아가고 있습니다.
           </li>
           <li>
-            2022년 기준 GitHub에 700번 이상의 <b>Commit</b> 을 진행했으며 React를
-            활용한 <b>웹 프론트엔드 개발</b>을 주로 하였고 반응형 디자인을 통한
-            웹앱 프로젝트 진행 경험이 있습니다. 최근엔 웹 성능 최적화나 웹
-            접근성을 고려한 개발에 관심을 가지고 있습니다.
+            서비스 최적화란 <b>웹 성능 최적화</b> 및 <b>UX 최적화</b>를 통해 이루어진다고 생각합니다.
+            <br className="hidden lg:inline-block" />
+            그렇기에, <b>렌더링 성능 개선</b>과 <b>웹 접근성 향상</b>에 대해 관심을 가지고 있습니다.
           </li>
           <li>
-            현재 2023년 <b>SSAFY</b>에서 알고리즘에 기반한 웹개발 교육 과정을
-            자바 전공자 반으로 수료 중에 있습니다.
-          </li>
-          <li>
-            <b>Slack</b>, <b>Jira</b>, <b>Gitlab</b> 등 협업 툴에 대한 경험이
-            있습니다.
+            현재, <b>삼성 청년 SW 아카데미</b>에서 팀 프로젝트를 진행하며
+            이를 <b>적용</b>할 방안에 대해 <b>고민</b>하고 <b>성장</b> 중에 있습니다.
           </li>
         </ul>
         <div className="w-full flex justify-end">
